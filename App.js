e623855cba218b66e1a40a1922bced657cfcf476
Tamas Erdos <github@tamaserdos.com>
2020-06-13 18:08:59 -0400

    initial

--- App.js
+++ App.js
@@ -1,19 +1,135 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableHighlight } from 'react-native';
+
+import Breathing from './Breathing'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [inCount, setInCount] = useState(4)
+  const [holdCount, setHoldCount] = useState(4)
+  const [outCount, setOutCount] = useState(4)
+  const [waitCount, setWaitCount] = useState(4)
+  const [breathMode, setBreathMode] = useState(false)
+
+  function toggleBreathMode() {
+    !!breathMode ? setBreathMode(false) : setBreathMode(new Date())
+  }
+
+  const settings = <View style={styles.rows}>
+    <Row label="In" n={inCount} setFunction={setInCount}/>
+    <Row label="Hold" n={holdCount} setFunction={setHoldCount}/>
+    <Row label="Out" n={outCount} setFunction={setOutCount}/>
+    <Row label="Wait" n={waitCount} setFunction={setWaitCount}/>
+  </View>
+
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        <Text style={styles.title}>Mindful Breathing</Text>
+        {breathMode ? <Breathing {...{inCount, holdCount, waitCount, outCount}} startTime={breathMode} /> : settings}
+        <View style={styles.buttonRow}>
+          <TouchableHighlight style={styles.button} onPress={toggleBreathMode}>
+            <Text style={styles.buttonText}>
+              {breathMode ? 'STOP' : 'START'}
+            </Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+}
+
+function Row(props) {
+  function padN(n) {
+    if (n < 10) {
+      return `0${n}`
+    }
+    return n
+  }
+
+  function incrementN() {
+    props.setFunction(props.n+1)
+  }
+
+  function decrementN() {
+    if (props.n > 0) {
+      props.setFunction(props.n-1)
+    }
+  }
+
+  return <View style={styles.row}>
+    <Text style={styles.heading}>Counts <Text style={styles.bold}>{props.label}</Text></Text>
+    <Text style={styles.number}>{padN(props.n)}</Text>
+    <TouchableHighlight style={styles.rowButton} onPress={incrementN}>
+      <Text style={styles.rowButtonText}>+</Text>
+    </TouchableHighlight>
+    <TouchableHighlight style={styles.rowButton} onPress={decrementN}>
+      <Text style={styles.rowButtonText}>-</Text>
+    </TouchableHighlight>
+  </View>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  rows: {
+    // flex: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 15,
+    marginTop: 15,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  heading: {
+    fontSize: 18,
+    width: 110,
+  },
+  number: {
+    fontSize: 64,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  rowButton: {
+    backgroundColor: 'black',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 10,
+  },
+  rowButtonText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonRow: {
+    marginBottom: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    padding: 40,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
+    // fontWeight: 'bold',
+  },
+  container: {
+    marginTop: 35,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
   },
 });
---
+++ Breathing.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, StatusBar, TouchableHighlight, Vibration } from 'react-native';
+
+import useInterval from './useInterval'
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
+
+export default function(props) {
+
+  const [stepText, setStepText] = useState('Breathe In')
+  const [timeText, setTimeText] = useState(0)
+  const [fill, setFill] = useState(0)
+
+  useInterval(() => {
+    breathingLoop()
+  }, 100)
+
+  useEffect(() => {
+    Vibration.vibrate(500)
+  }, [stepText])
+
+  const totalTime = (props.holdCount + props.inCount + props.outCount + props.waitCount) * 1000
+
+  function breathingLoop() {
+    const secs = Math.round((new Date() - props.startTime)) % totalTime
+    setTimeText(secs)
+    setFill(secs / totalTime * 100)
+
+    if (secs / 1000 < props.inCount) { // in
+      setStepText('Breathe In')
+      setFill(secs / (props.inCount * 1000) * 100)
+
+    } else if (secs / 1000 < props.holdCount + props.inCount) { // hold
+      setStepText('Hold')
+      setFill((secs - props.inCount * 1000) / (props.holdCount * 1000) * 100)
+
+    } else if (secs / 1000 < props.outCount + props.inCount + props.holdCount) { // out
+      setStepText('Breathe Out')
+      setFill((secs - (props.inCount + props.holdCount) * 1000) / (props.outCount * 1000) * 100)
+
+    } else { // wait
+      setStepText('Wait')
+      setFill((secs - (props.inCount + props.holdCount + props.outCount) * 1000) / (props.waitCount * 1000) * 100)
+
+    }
+  }
+
+
+  return <View style={styles.container}>
+    <Text style={styles.instruction}>{stepText}</Text>
+    <AnimatedCircularProgress
+    size={200}
+    width={100}
+    fill={fill}
+    tintColor="#999"
+    backgroundColor="#fff" />
+  </View>
+}
+
+const styles = {
+  instruction: {
+    fontSize: 42,
+    textAlign: 'center',
+    fontWeight: '100',
+    marginBottom: 20,
+    // fontStyle: 'italic'
+  },
+  container: {
+    alignItems: 'center',
+  },
+}
